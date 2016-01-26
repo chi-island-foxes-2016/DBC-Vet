c78647c0587ce6f52cd78aa5acee1397bebac339
Parminder Singh <email": null>
2016-01-26 13:10:15 -0600

    document controller, show,edit pages done

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,11 +4,15 @@ class DocumentsController < ApplicationController
     @documents = Document.all
   end
 
+  def show
+    @document = Document.find(params[:id])
+  end
+
   def new
     @document = Document.new
   end
 
   def edit
-    @document = Document.find()
+    @document = Document.find(params[:id])
   end
 end
