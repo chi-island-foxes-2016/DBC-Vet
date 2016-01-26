78b1b3202914e9ca1b91fdbdca74fbecbf3203b9
Parminder Singh <email": null>
2016-01-26 09:46:08 -0600

    fix a spelling error

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   has_many :users_groups
   has_many :users, through: :users_groups
-  has_many :documents_grops
+  has_many :documents_groups
   has_many :documents
 end
