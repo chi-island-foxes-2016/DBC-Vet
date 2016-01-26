10c7ada0f5ea5b784b8269680fc2b2348fed28e4
Parminder Singh <email": null>
2016-01-26 09:56:11 -0600

    fix changed name in the sessions controller and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user  = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to manuals_path
+      redirect_to documents_path
     else
       redirect_to signup_path
     end
