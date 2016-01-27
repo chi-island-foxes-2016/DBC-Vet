a4ecb84c3d6255f51448b2f3feb1d808734965ba
Pratik Shah <pratik.t.shah@gmail.com>
2016-01-26 22:25:43 -0600

    User can be added to group

---
+++ app/assets/javascripts/form_group.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  var test = $("#user_group_id").attr('name', 'group[group_type]');
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user  = User.new(user_params)
-    if @user.save
+    @group = @user.groups.new(group_params)
+    if @user.save && @group.save
       session[:user_id] = @user.id
       redirect_to documents_path
     else
@@ -25,12 +26,16 @@ class UsersController < ApplicationController
       redirect_to group_path(@user.groups[0].id)
     else
       redirect_to edit_path
-    end 
+    end
   end
   #this is required by rails 4
   private
 
+  def group_params
+    params.require(:group).permit(:group_type)
+  end
+
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation, :admin, :group_type)
   end
 end
