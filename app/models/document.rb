4bbf6e643423572dc8798a7e578aecba46acb12c
Timothy Meixell <misternu@gmail.com>
2016-01-26 19:32:46 -0600

    Fix document creation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,8 @@
 class Document < ActiveRecord::Base
 	belongs_to :group
 	has_many :document_groups
+
+  validates :title, presence: true
+  validates :group_id, presence: true
+  validates :content, presence: true
 end
